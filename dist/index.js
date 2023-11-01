59be52f63e71b18480cd40017754aa3354c6056c
lookB4Uleap <anish.sarkar.official@gmail.com>
2023-11-01 13:58:38 +0530

    Added express json

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
+app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Express + TypeScript Server');
 });
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ const app: Express = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
