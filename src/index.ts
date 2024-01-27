2e981ad50fe4c0cbea1279df2f92f9b98056ba39
lookB4Uleap <anish.sarkar.official@gmail.com>
2024-01-27 13:28:06 +0530

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,37 @@
-import express, {Express, Request, Response} from 'express';
+import express, { Express, Request, Response } from 'express';
 import 'dotenv/config'; // remember
 import cors from 'cors';
+import cluster from 'cluster';
+import os from 'node:os';
 
-const app: Express = express();
-const port = process.env.PORT || 5000;
+const numCPUs = os.cpus().length;
 
-app.use(cors());
-app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
-});
+if (cluster.isPrimary) {
+    console.log(`Master process ${process.pid} is running`);
 
-app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`Worker process ${worker.process.pid} died. Restarting...`);
+        cluster.fork();
+    });
+} else {
+    const app: Express = express();
+    const port = process.env.PORT || 5000;
+
+    app.use(cors());
+    app.use(express.json());
+
+    app.get('/', (req: Request, res: Response) => {
+		console.log(`Request processed at ${process.pid}`);
+        res.send('Express + TypeScript Server');
+    });
+
+    app.listen(port, () => {
+        console.log(`[server]: Server is running at http://localhost:${port}`);
+    });
+
+}
